0ece4f413b4480d07bbd854860a9f6f4cf5299ea
y-okubo <yokubo@nekojarashi.com>
2015-11-11 14:46:46 +0900

    Reset code

--- src/com/seagate/kinetic/example/KineticHelloWorld.java
+++ src/com/seagate/kinetic/example/KineticHelloWorld.java
@@ -1,60 +1,64 @@
 /**
- *
+ * 
  * Copyright (C) 2014 Seagate Technology.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
+
 /**
- * Copyright (c) 2013 Seagate Technology LLC All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1) Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2) Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3) Neither the name of Seagate Technology nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission from Seagate Technology.
- *
- * 4) No patent or trade secret license whatsoever, either express or implied,
- * is granted by Seagate Technology or its contributors by this copyright
- * license.
- *
- * 5) All modifications must be reposted in source code form in a manner that
- * allows user to readily access the source code.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE COPYRIGHT HOLDERS AND
- * CONTRIBUTORS DISCLAIM ALL LIABILITY FOR INTELLECTUAL PROPERTY INFRINGEMENT
- * RELATED TO THIS SOFTWARE.
+ * Copyright (c) 2013 Seagate Technology LLC
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+
+ * 1) Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+
+ * 2) Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+
+ * 3) Neither the name of Seagate Technology nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission
+ * from Seagate Technology.
+
+ * 4) No patent or trade secret license whatsoever, either express or implied, is granted by Seagate
+ * Technology or its contributors by this copyright license.
+
+ * 5) All modifications must be reposted in source code form in a manner that allows user to
+ * readily access the source code.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS DISCLAIM ALL LIABILITY FOR
+ * INTELLECTUAL PROPERTY INFRINGEMENT RELATED TO THIS SOFTWARE.
  */
+
 package com.seagate.kinetic.example;
 
 import kinetic.client.ClientConfiguration;
@@ -62,6 +66,7 @@ import kinetic.client.Entry;
 import kinetic.client.KineticClient;
 import kinetic.client.KineticClientFactory;
 import kinetic.client.KineticException;
+import kinetic.simulator.KineticSimulator;
 import kinetic.simulator.SimulatorConfiguration;
 
 /**
@@ -80,38 +85,42 @@ import kinetic.simulator.SimulatorConfiguration;
  */
 public class KineticHelloWorld {
 
-    public static void main(String[] args) throws KineticException {
+	public static void main(String[] args) throws KineticException {
 
-        // Simulator configuration and initialization
-        SimulatorConfiguration simulatorConf = new SimulatorConfiguration();
+		// Simulator configuration and initialization
+		SimulatorConfiguration simulatorConf = new SimulatorConfiguration();
 
-        // Use memory store.
-        simulatorConf.setUseMemoryStore(true);
+		// Use memory store.
+		simulatorConf.setUseMemoryStore(true);
 
-        // Client configuration and initialization
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        KineticClient client = KineticClientFactory.createInstance(clientConfig);
+		KineticSimulator simulator = new KineticSimulator(simulatorConf);
 
-        // Create the entry key/value
-        byte[] key = "Hello".getBytes();
-        byte[] value = "World".getBytes();
-        Entry entry = new Entry(key, value);
+		// Client configuration and initialization
+		ClientConfiguration clientConfig = new ClientConfiguration();
+		KineticClient client = KineticClientFactory
+				.createInstance(clientConfig);
 
-        // Put entry ignore version
-        client.putForced(entry);
-        System.out.printf("put key=\"%s\" value=\"%s\"\n",
-        new String(key), new String(value));
+		// Create the entry key/value
+		byte[] key = "Hello".getBytes();
+		byte[] value = "World".getBytes();
+		Entry entry = new Entry(key, value);
 
-        // Get key
-        Entry entryGet = client.get(key);
-        System.out.printf("get key=\"%s\" retrieved value=\"%s\"\n",
-        new String(key), new String(entryGet.getValue()));
+		// Put entry ignore version
+		client.putForced(entry);
+		System.out.printf("put key=\"%s\" value=\"%s\"\n",
+				new String(key), new String(value));
 
-        // Delete entry
-        client.delete(entry);
-        System.out.printf("delete key=\"%s\"\n", new String(key));
+		// Get key
+		Entry entryGet = client.get(key);
+		System.out.printf("get key=\"%s\" retrieved value=\"%s\"\n",
+				new String(key), new String(entryGet.getValue()));
 
-        // Close client and simulator
-        client.close();
-    }
+		// Delete entry
+		client.delete(entry);
+		System.out.printf("delete key=\"%s\"\n", new String(key));
+
+		// Close client and simulator
+		client.close();
+		simulator.close();
+	}
 }
